045b1550a97f55f49c1ad82858072f19fe02925a
Annielli Rangel <anniellirangel@gmail.com>
2022-12-03 23:26:42 -0300

    solved

--- routes/album.router.js
+++ routes/album.router.js
@@ -1,9 +1,73 @@
-import express from "express"
-import AlbumModel from "../models/album.model.js"
-import PurchaseModel from "../models/purchase.model.js"
+import express from "express";
+import AlbumModel from "../models/album.model.js";
+import PurchaseModel from "../models/purchase.model.js";
 
-const albumRoute = express.Router()
+const albumRoute = express.Router();
 
+//2.1 Crie a rota POST /albums
+albumRoute.post("/creat-album", async (req, res) => {
+  try {
+    const newalbum = await AlbumModel.create(req.body);
+    return res.status(201).json(newalbum);
+  } catch (error) {
+    console.log(error);
+    return res._construct(500).json(error);
+  }
+});
 
+//2.2 Crie a rota GET /albums
+albumRoute.get("/all-albuns", async (req, res) => {
+  try {
+    const allAlbuns = await AlbumModel.find();
+    return res.status(200).json(allAlbuns);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+});
 
-export default albumRoute
+//2.3 Crie a rota GET /albums/:albumId
+albumRoute.get("/oneAlbum/:albumId", async (req, res) => {
+  try {
+    const { albumId } = req.params;
+    const album = await AlbumModel.findById(albumId);
+    return res.status(200).json(album);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+});
+
+//2.4 Crie a rota PUT /albums/:albumId
+
+albumRoute.put("/edit/albumId", async (req, res) => {
+  try {
+    const { albumId } = req.params;
+    const updatedAlbum = await AlbumModel.findByIdAndUpdate(
+      albumId,
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
+    return res.status(200).json(updatedAlbum);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+});
+
+//2.5 Crie a rota DELETE /albums/:albumId
+albumRoute.delete("/delete/:albumId", async (req, res) => {
+  try {
+    const { albumId } = req.params;
+    const deletedAlbum = await AlbumModel.findByIdAndDelete(albumId);
+        if(!deletedAlbum){
+            return res.status(400).json({msg:"Don't find"})
+        }
+    return res.status(200).json(deletedAlbum);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+});
+
+export default albumRoute;
--- routes/purchase.router.js
+++ routes/purchase.router.js
@@ -1,7 +1,32 @@
-import express from "express"
-import AlbumModel from "../models/album.model.js"
-import PurchaseModel from "../models/purchase.model.js"
+import express from "express";
+import AlbumModel from "../models/album.model.js";
+import PurchaseModel from "../models/purchase.model.js";
 
-const purchaseRoute = express.Router()
+const purchaseRoute = express.Router();
 
-export default purchaseRoute
+//3.1 Crie a rota POST /purchases
+
+purchaseRoute.post("/new-purchase/", async (req, res) => {
+  try {
+    const newPurchase = await PurchaseModel.create(req.body);
+
+    return res.status(201).json(newPurchase);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+});
+
+//3.2 Crie a rota GET /purchases/:purchaseId
+purchaseRoute.get("/onePurchase/:purchaseId", async (rq, res) => {
+  try {
+    const { purchaseId } = req.params;
+    const purchase = await PurchaseModel.findById(purchaseId).populate("album");
+    return res.status(200).json(purchase);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error);
+  }
+});
+
+export default purchaseRoute;
